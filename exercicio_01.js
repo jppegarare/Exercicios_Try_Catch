79cf2f530e0e3b2adca06f457f8883dbece2b257
João Pegarare <jppegarare18@gmail.com>
2023-06-07 17:09:05 -0400

    Terminei o exercício 1 e comecei o 2

--- exercicio_01.js
+++ exercicio_01.js
@@ -1,6 +1,12 @@
 /*
   Os atributos nomes, idade e especie são todos necessários. Assim, usando Try e Catch trate os erros e apresente uma mensagem de erro quando houve atributos vazios.
 */
+class MeuErro extends Error {
+  constructor(message){
+    super(message);
+    this.name = "Erro nos Atributos";
+  }
+}
 
 class Animal {
   constructor(nome, idade, especie) {
@@ -27,17 +33,18 @@ class Animal {
     }
     else
     {
-        throw new Error("Insira o nome, idade e especie para continuar :)")
+        throw new MeuErro("Insira o nome, idade e especie para continuar :)")
     }
   }
 }
 
-const meuAnimal = new Animal("Fido", 3, "cachorro");
+const meuAnimal = new Animal("", 3, "cachorro");
 const atributos = meuAnimal.mostrarAtributos();
+console.log(atributos)
 
 // Imprimindo os atributos
-console.log(atributos.nome);   
-console.log(atributos.idade);  
-console.log(atributos.especie); 
-console.log(atributos)
+// console.log(atributos.nome);   
+// console.log(atributos.idade);  
+// console.log(atributos.especie); 
+
 
--- exercicio_02.js
+++ exercicio_02.js
@@ -2,6 +2,8 @@
   Todos os atributos necessários. Assim, usando Try e Catch, trate os erros e apresente uma mensagem quando houve atributos vazios.
 */
 
+class MeuErro extends Error
+
 class Estudante {
   constructor(nome, idade, turma) {
     this.nome = nome;
